03e9fea3e523bd480d42dcf200049f11f3e3faa5
Kunal Kumar <kmrknl2604@gmail.com>
2023-02-04 20:45:51 +0530

    HttpMethods, JsonStringEnumConverter

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -8,5 +8,30 @@ namespace netcorewebapi.Controllers
     public class CharacterController : ControllerBase
     {
         private static Character knight = new Character();
+        private static List<Character> characters = new List<Character>()
+        {
+            new Character(),
+            new Character{ Name = "Trisha", Inteligence = 20, Class = RpgClass.Mage }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(knight);
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get2()
+        {
+            return Ok(knight);
+        }
+
+        // Combine route in the same line
+        [HttpGet("GetCharacters")] 
+        public ActionResult<List<Character>> GetCharacters()
+        {
+            return Ok(characters);
+        }
     }
 }
--- Models/RpgClass.cs
+++ Models/RpgClass.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace netcorewebapi.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RpgClass
     {
         Knight = 1,
