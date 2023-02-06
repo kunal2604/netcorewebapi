709fda8d0897bfa78aa5196ca01aa65f5ba1dd3b
Kunal Kumar <kmrknl2604@gmail.com>
2023-02-07 02:11:34 +0530

    Dependency Injection

---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,11 @@
+namespace netcorewebapi.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+        
+        Character GetCharacterById(int id);
+
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
