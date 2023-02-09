9d3dccf84155ff7257e3eebbac7923aae8f4896f
Kunal Kumar <kmrknl2604@gmail.com>
2023-02-10 04:36:52 +0530

    CRUD with SQL server using Entity Framework

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -9,34 +9,39 @@ namespace netcorewebapi.Services.CharacterService
         };
 
         private readonly IMapper _mapper;
-        public CharacterService(IMapper mapper)
+        private readonly DataContext _context;
+
+        public CharacterService(IMapper mapper, DataContext context)
         {
             _mapper = mapper;
+            _context = context; 
         }
 
         public async Task<ServiceResponse<List<GetCharacterResponseDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterResponseDto>>();
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterResponseDto>(c)).ToList();
+            var dbCharacters = await _context.Characters.ToListAsync();
+            serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterResponseDto>(c)).ToList();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<GetCharacterResponseDto>> GetCharacterById(int id)
         {
-            var character = characters.FirstOrDefault(c => c.Id == id);
             var serviceResponse = new ServiceResponse<GetCharacterResponseDto>();
-            serviceResponse.Data = _mapper.Map<GetCharacterResponseDto>(character);
+
+            //var dbCharacter = await _context.Characters.FindAsync(id);
+            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            
+            serviceResponse.Data = _mapper.Map<GetCharacterResponseDto>(dbCharacter);
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetCharacterResponseDto>>> AddCharacter(AddCharacterRequestDto newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterResponseDto>>();
-            var character = _mapper.Map<Character>(newCharacter);
-            character.Id = characters.Max(c => c.Id) + 1;
-            characters.Add(character);
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterResponseDto>(c)).ToList();
-            return serviceResponse;
+            _context.Characters.Add(_mapper.Map<Character>(newCharacter));
+            await _context.SaveChangesAsync();
+            return await GetAllCharacters();
         }
 
         public async Task<ServiceResponse<GetCharacterResponseDto>> UpdateCharacter(UpdateCharacterRequestDto updatedCharacter)
@@ -44,16 +49,21 @@ namespace netcorewebapi.Services.CharacterService
             var serviceResponse = new ServiceResponse<GetCharacterResponseDto>();
             try
             {
-            var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
-            if(character is null)
-            {
-                throw new Exception($"Character with Id '{updatedCharacter.Id}' not found !!");
-            }
+                var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
+                if(dbCharacter is null)
+                {
+                    throw new Exception($"Character with Id '{updatedCharacter.Id}' not found !!");
+                }
             
-            _mapper.Map(updatedCharacter, character);
-
-            serviceResponse.Data = _mapper.Map<GetCharacterResponseDto>(character);
-            
+                dbCharacter.Name = updatedCharacter.Name;
+                dbCharacter.HitPoints = updatedCharacter.HitPoints;
+                dbCharacter.Inteligence = updatedCharacter.Inteligence;
+                dbCharacter.Strength = updatedCharacter.Strength;
+                dbCharacter.Defense = updatedCharacter.Defense;
+                dbCharacter.Class = updatedCharacter.Class;
+                
+                await _context.SaveChangesAsync();
+                return await GetCharacterById(updatedCharacter.Id);
             }
             catch(Exception ex)
             {
@@ -69,13 +79,14 @@ namespace netcorewebapi.Services.CharacterService
             var serviceResponse = new ServiceResponse<List<GetCharacterResponseDto>>();
             try
             {
-                var character = characters.FirstOrDefault(c =>c.Id == id);
-                if(character is null)
+                var dbCharacter = await _context.Characters.FindAsync(id);
+                if(dbCharacter is null)
                 {
                     throw new Exception($"Character with id '{id}' was not found !!");
                 }
-                characters.Remove(character);
-                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterResponseDto>(c)).ToList();
+                _context.Characters.Remove(dbCharacter);
+                await _context.SaveChangesAsync();
+                return await GetAllCharacters();
             }
             catch(Exception ex)
             {
